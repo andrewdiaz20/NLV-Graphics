85e00ac722e020bc5755ee7741f7246b0004251a
Andrew Diaz <andrewdiazn20@gmail.com>
2024-06-04 11:55:22 -0700

    Homescreen skeleton set up

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,23 @@
+import NavBar from './Navigation';
+
+function Home() {
+  return (
+    <body>
+        <NavBar/>
+      <main>
+        <img></img>
+      </main>
+      <section>
+        <p>hello</p>
+      </section>
+      <section>
+        <p>welcome</p>
+      </section>
+      <section>
+
+      </section>
+    </body>
+  );
+}
+
+export default Home;
