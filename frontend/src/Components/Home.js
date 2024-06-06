c70fe8bb6bdfe894bb6f3c3f4f9ea3b133cf750d
Andrew Diaz <andrewdiazn20@gmail.com>
2024-06-05 17:02:51 -0700

    Home screen 50% done

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -3,21 +3,25 @@ import NavBar from './Navigation';
 function Home() {
   return (
     <body>
-        <NavBar/>
-      <main>
-        <img></img>
+      <NavBar/>
+      <main className='homePICTURE'>
+        <img src='maxresdefault.jpg'alt='8 best AI TOOLS for designers'/>
       </main>
-      <section>
-        <p>hello</p>
+      <section className='liveInColor'>
+        <img/>
       </section>
-      <section>
-        <p>welcome</p>
+      <section className='aboutUS'>
+        <h2>About Us</h2>
+        <div className='aboutInfo'>
+        <p className='aboutUSP'>
+        NLV Graphics is a printing and graphics shop that provides high-quality printing services for all your needs. We specialize in custom apparel printing, including T-shirts, sweatshirts, hats, and more. We also offer laser engraving services for insulated drinkware and glassware, custom die-cut stickers, posters, signs, and large-format printing. Our team of experts can put your logo on almost anything. In addition to printing services, we provide graphic design services and laser engraving services to make your product stand out. Contact us today to learn more about our services and how we can help you with your printing needs.</p>
+        <img src='OIP.jpg'alt='Motion Graphics'/>
+        </div>
       </section>
       <section>
 
       </section>
     </body>
   );
-}
-
+} 
 export default Home;
--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -2,14 +2,26 @@ import React from "react";
 
 function NavBar() {
     return (
-        <nav>
-            <ul>
-                <li>HOME</li>
-                <li>Stickers & Labels</li>
-                <li>Drinkware</li>
-                <li>Banners</li>
-                <li>Large Format Printing</li>
-                <li>CONTACT</li>
+        <nav className="Navigation">
+            <ul className="NavBAR">
+                <li className="NavLINK">
+                    <a href="/">Home</a>
+                </li>
+                <li className="NavLINK">
+                    <a href="/Stickers.Labels">Stickers & Labels</a>
+                </li>
+                <li className="NavLINK">
+                    <a href="/Drinkware">Drinkware</a>
+                </li>
+                <li className="NavLINK">
+                    <a href="/Banners">Banners</a>
+                </li>
+                <li className="NavLINK">
+                    <a href="/Large.Format.Printing">Large Format Printing</a>
+                </li>
+                <li className="NavLINK">
+                    <a href="/Contact">Contact</a>
+                </li>
             </ul>
         </nav>
     );
