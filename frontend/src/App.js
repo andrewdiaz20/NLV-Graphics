45cb8cdcc2ef85e3afe3831417def00b47bdab38
DannniF <fdaniel0819@gmail.com>
2024-06-10 22:55:16 -0700

    Installed Bootstrap react, tested by adding bootstrap forms and cards to contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Home from './Components/Home';
+import Contact from './Components/Contact';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import NavBar from './Components/Navigation';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
       <Routes>
         <Route>
           <Route path = '/'element={<Home/>}/>
+          <Route path = '/Contact' element={<Contact/>}/>
         </Route>
       </Routes>
     </Router>
--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -1,9 +1,47 @@
-import React from "react";
+import NavBar from './Navigation'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
-function Footer(){
-    return(
-        <main></main>
+function Contact() {
+    return (
+        <body>
+            {/* <NavBar/> */}
+            <main>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some Quick Example Text to Build on the card Title and amke up the bulk of the cards content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Title>Information</Card.Title>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Card>
+            </main>
+        </body>
     )
 };
 
-export default Footer;
+export default Contact;
