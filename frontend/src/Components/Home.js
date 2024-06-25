c46de0c009b93ccf93adb4919814a2597ec03e15
Andrew Diaz <andrewdiazn20@gmail.com>
2024-06-25 15:00:00 -0700

    who we are

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -36,10 +36,20 @@ function Home() {
 
       {/* End of About us section */}
 
-      <section>
-        <h2>Who We Are</h2>
+      <section className='whoweAre'>
+        <header className='whoareweHeaders'>
+          <h2>Who We Are</h2>
+          </header>
+        <div className='whoareweLeft'>
+          i am left side bar
+        </div>
+        <div className='whoareweText'>
         <p>This is your About section. This space is a great opportunity to give a full background on who you are, what you do and what your site has to offer. Your users are genuinely interested in learning more about you, so don’t be afraid to share personal anecdotes to create a more friendly quality.</p>
         <p>Double click on the text box to start editing your content and make sure to add all the relevant details you want site visitors to know. If you’re a business, talk about how you started and share your professional journey. Explain your core values, your commitment to customers and how you stand out from the crowd. Add a photo, gallery or video for even more engagement.</p>
+        </div>
+        <div className='whoareweRight'>
+          i am right side bar
+        </div>
       </section>
       <Footer/>
     </body>
