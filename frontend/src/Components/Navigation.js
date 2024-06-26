47782287a8412f60c088cac60d60974ed3ff5ab0
Andrew Diaz <andrewdiazn20@gmail.com>
2024-06-26 15:25:45 -0700

    Font change and logo inserted into nav bar

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -5,6 +5,9 @@ function NavBar() {
         <div id="header-wrap">
             <nav id="header" className="Navigation">
                 <ul className="NavBAR">
+                    <li>
+                        <img className="nlvGraphicsLogo"src="0f542a_923835a140ab4c7d8333171021b708ae~mv2.webp" alt="NLV-Graphics logo"/>
+                    </li>
                     <li className="NavLINK">
                         <a href="/">Home</a>
                     </li>
