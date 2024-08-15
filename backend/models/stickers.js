873e15138e17ce5e9ba8e58008cd88898d9434d2
Andrew Diaz <andrewdiazn20@gmail.com>
2024-08-15 15:25:03 -0700

    Stickers

--- backend/models/stickers.js
+++ backend/models/stickers.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const stickerSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    pic: String,
+    name: String,
+    description: String,
     price: Number
-})
+});
 
 module.exports = mongoose.model('sticker', stickerSchema)
