66e85263afb6d9b09c633a57955d2e4a08872a25
DannniF <fdaniel0819@gmail.com>
2024-09-01 13:45:22 -0700

    changed somthing

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,6 @@ const stickers = require('./models/stickers');
 // const { default: Stickers } = require('../frontend/src/Components/stickers');
 
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB', err));
 
 const sticker = (req, res, next) => {
     // Your middleware logic here
@@ -32,6 +26,12 @@ app.use('/stickers', stickers);
 //     }
 // });
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
