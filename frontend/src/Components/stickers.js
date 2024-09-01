ecc5b0155907d9339bfd08578b890623c5c0ca93
Andrew Diaz <andrewdiazn20@gmail.com>
2024-09-01 13:44:07 -0700

    progres

--- frontend/src/Components/stickers.js
+++ frontend/src/Components/stickers.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
+import { useParams, useNavigate, Link} from "react-router-dom"
 import Navbar from "./Navigation";
 import Footer from "./Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
-import { useNavigate, useParams } from 'react-router-dom';
+
 
 function Stickers() {
   const [sticker, setSticker] = useState([]);
-  const { id } = useParams();
-  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchData() {
-      const url = `${process.env.REACT_APP_BACKEND_URL}/sticker/${id}`;
+      const url = `${process.env.REACT_APP_BACKEND_URL}/sticker/`;
       const response = await fetch(url);
       const data = await response.json();
       setSticker(data);
@@ -22,7 +21,7 @@ function Stickers() {
       console.log('Hello world')
     }
     fetchData();
-  }, [id]);
+  }, []);
 
   return (
     <div className="stickersBody">
