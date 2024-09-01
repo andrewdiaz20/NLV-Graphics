fe40911b8fd6105ba9bcef93921ddb36ece36fa8
Andrew Diaz <andrewdiazn20@gmail.com>
2024-09-01 12:25:38 -0700

    stickers

--- frontend/src/Components/stickers.js
+++ frontend/src/Components/stickers.js
@@ -1,43 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Navbar from "./Navigation";
 import Footer from "./Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function Stickers() {
-  const [stickers, setStickers] = useState([]);
+  const [sticker, setSticker] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('http://localhost:8080/sticker')
-      .then(response => setStickers(response.data))
-      .catch(error => console.error('error:', error));
-  }, []);
-
-  const fetchStickers = async () => {
-    const url = `${process.env.REACT_APP_BACKEND_URL}/sticker`;
-    // Add your fetch logic here
-  };
+    async function fetchData() {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/sticker/${id}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setSticker(data);
+      console.log(data);
+      console.log('Hello world')
+    }
+    fetchData();
+  }, [id]);
 
   return (
     <div className="stickersBody">
       <Navbar />
       <main className="headerStickers">
         <h1>Stickers & Labels</h1>
-      </main>
-      <div>
-        {stickers.map(sticker => (
-          <div key={sticker._id}>
-            <h2>{sticker.name}</h2>
-            <p>{sticker.description}</p>
-            <p>{sticker.price}</p>
-          </div>
+        {sticker.map((sticker) => (
+          <Card className='stickersCss' key={sticker.id} sx={{ maxWidth: 800 }}>
+            <div style={{ position: 'relative' }}>
+              <p>{sticker.name}</p>
+            </div>
+          </Card>
         ))}
-      </div>
+      </main>
       <Footer />
     </div>
   );
 }
 
 export default Stickers;
+
